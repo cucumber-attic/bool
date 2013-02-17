9eb95d2c875571a27b95b5e7ed0360fb19f08ed2
Ilan Pillemer <ilan.pillemer@gmail.com>
2013-02-17 20:16:48 +0200

    don't need brackets for the not. ref #32

--- java/src/main/java/bool/ExplicitVisitor.java
+++ java/src/main/java/bool/ExplicitVisitor.java
@@ -21,7 +21,7 @@ public class ExplicitVisitor implements Visitor< String, List<String>> {
 
 	@Override
 	public String not(Not not, List<String> notused) {
-		return "!(" + explicit(not.operand) + ")"; 
+		return "!" + explicit(not.operand); 
 	}
 	
 	private String explicit (Expr expr) {

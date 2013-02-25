07bbb7990d6a23e18c51079f13c76e5e60253fec
Ilan Pillemer <ilan.pillemer@gmail.com>
2013-02-25 21:38:06 +0200

    untabified to follow convention.. see #33

--- java/src/main/java/bool/Renderer.java
+++ java/src/main/java/bool/Renderer.java
@@ -4,28 +4,28 @@ import java.util.List;
 
 public class Renderer implements Visitor< String, List<String>> {
 
-	@Override
-	public String var(Var var, List<String> notused) {
-		return var.name;
-	}
+    @Override
+    public String var(Var var, List<String> notused) {
+        return var.name;
+    }
 
-	@Override
-	public String and(And and, List<String> notused) {
-		return "(" + explicit(and.left) + " && " + explicit(and.right) + ")";
-	}
+    @Override
+    public String and(And and, List<String> notused) {
+        return "(" + explicit(and.left) + " && " + explicit(and.right) + ")";
+    }
 
-	@Override
-	public String or(Or or, List<String> notused) {
-		return "(" + explicit(or.left) + " || " + explicit(or.right) + ")";
-	}
+    @Override
+    public String or(Or or, List<String> notused) {
+        return "(" + explicit(or.left) + " || " + explicit(or.right) + ")";
+    }
 
-	@Override
-	public String not(Not not, List<String> notused) {
-		return "!" + explicit(not.operand); 
-	}
-	
-	private String explicit (Expr expr) {
-	    return expr.describeTo(this, null);
-	}
+    @Override
+    public String not(Not not, List<String> notused) {
+        return "!" + explicit(not.operand); 
+    }
+    
+    private String explicit (Expr expr) {
+        return expr.describeTo(this, null);
+    }
 
 }
--- java/src/test/java/bool/RendererTest.java
+++ java/src/test/java/bool/RendererTest.java
@@ -3,40 +3,31 @@ package bool;
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.CoreMatchers.*;
 import org.junit.Test;
-
 import java.io.IOException;
 
-import static java.util.Arrays.asList;
-import static org.junit.Assert.assertTrue;
-
-
-
 public class RendererTest {
-	@Test
-	public void test_and_or_expression() throws IOException {
-		Parser parser = new Parser(new Lexer("a && b || c"));
-		Expr expr = parser.parseExpr();
-		assertThat("a && b || c should explicitly be ((a && b) || c)",
-				expr.describeTo(new Renderer(),null),
-				is ("((a && b) || c)"));
-		
-	}
-	
-	@Test
-	public void test_or_and_expression() throws IOException {
-		Parser parser = new Parser(new Lexer("a || b && c"));
-		Expr expr = parser.parseExpr();
-		assertThat("a || b && c should explicitly be (a || (b && c))",
-				expr.describeTo(new Renderer(),null),
-				is ("(a || (b && c))"));
-	}
-	
-	@Test
-	public void test_not_expression() throws IOException {
-		Parser parser = new Parser(new Lexer("!(a || b && !c)"));
-		Expr expr = parser.parseExpr();
-		assertThat("!(a || b && !c) should explicitly be !(a || (b && !c))",
-				expr.describeTo(new Renderer(),null),
-				is ("!(a || (b && !c))"));
-	}
+    @Test
+    public void test_and_or_expression() throws IOException {
+        Parser parser = new Parser(new Lexer("a && b || c"));
+        Expr expr = parser.parseExpr();
+        assertThat("a && b || c should explicitly be ((a && b) || c)",
+                expr.describeTo(new Renderer(), null), is("((a && b) || c)"));
+
+    }
+
+    @Test
+    public void test_or_and_expression() throws IOException {
+        Parser parser = new Parser(new Lexer("a || b && c"));
+        Expr expr = parser.parseExpr();
+        assertThat("a || b && c should explicitly be (a || (b && c))",
+                expr.describeTo(new Renderer(), null), is("(a || (b && c))"));
+    }
+
+    @Test
+    public void test_not_expression() throws IOException {
+        Parser parser = new Parser(new Lexer("!(a || b && !c)"));
+        Expr expr = parser.parseExpr();
+        assertThat("!(a || b && !c) should explicitly be !(a || (b && !c))",
+                expr.describeTo(new Renderer(), null), is("!(a || (b && !c))"));
+    }
 }

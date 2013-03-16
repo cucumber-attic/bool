1b73db760c6ba272c5c696f714bd0ca996c76d68
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2013-03-16 06:58:40 +0000

    Comment

--- ruby/resolv.rb
+++ ruby/resolv.rb
@@ -1,3 +1,6 @@
+# This file will not be packaged in the gem.
+# It is only used during build, to work around this issue:
+# https://github.com/cucumber/bool/issues/37
 module Win32
   module Resolv
     def get_hosts_path

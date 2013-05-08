eac4cfdb9436044816e11258c85262808965dd09
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2013-05-08 17:50:34 -0500

    More thorough test

--- javascript/test/compiler_test.js
+++ javascript/test/compiler_test.js
@@ -21,6 +21,8 @@ describe('Compiler', function() {
     var units = new Compiler().compile(feature);
     assert.equal(3, units.length);
     // TODO: assert.equal([???], units[0].steps[0].stack_frames)
-    assert.equal('I have 1 cukes in my belly', units[0].steps[0].name.value);
+    assert.equal('I have 20 cukes (20) in my belly', units[1].steps[0].name.value);
+    assert.equal('I eat 20 cukes', units[1].steps[1].name.value);
+    assert.equal('I should have 40 cukes in my belly', units[1].steps[2].name.value);
   });
 });

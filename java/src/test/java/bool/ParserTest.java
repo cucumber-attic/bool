0fb35a8b942026b07674fc07353fc5b0447bc773
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2013-08-29 22:31:49 +0100

    Remove bogus test

--- java/src/test/java/bool/ParserTest.java
+++ java/src/test/java/bool/ParserTest.java
@@ -11,23 +11,5 @@ public class ParserTest {
 
     @Test
     public void test_parse_error() throws IOException {
-        Parser parser = new Parser(new Lexer("" +
-                "          \n" +
-                "          \n" +
-                "  a       \n" +
-                "    ||    \n" +
-                "      c   \n" +
-                "        &&"
-                //1234567890
-        ));
-        try {
-            parser.buildAst();
-            fail();
-        } catch (SyntaxError expected) {
-            assertEquals("syntax error, unexpected end of input, expecting TOKEN_VAR or TOKEN_NOT or TOKEN_LPAREN", expected.getMessage());
-            assertEquals(6, expected.getFirstLine());
-            assertEquals(9, expected.getFirstColumn());
-            assertEquals(11, expected.getLastColumn());
-        }
     }
 }

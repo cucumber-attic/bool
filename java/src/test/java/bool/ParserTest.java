1e376aef4e1f9dd54522f89acd4e609d40f6a9a2
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2013-09-03 22:40:39 +0100

    Started on parser tests

--- java/src/test/java/bool/ParserTest.java
+++ java/src/test/java/bool/ParserTest.java
@@ -1,5 +1,6 @@
 package bool;
 
+import bool.ast.Feature;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -10,6 +11,10 @@ import static org.junit.Assert.fail;
 public class ParserTest {
 
     @Test
-    public void test_parse_error() throws IOException {
+    public void parses_simple_feature() throws IOException {
+        Parser parser = new Parser(new Lexer("Feature: foo"));
+        Feature feature = parser.buildAst();
+        assertEquals("foo", feature.name.getValue());
+        assertEquals(10, feature.name.getFirstColumn());
     }
 }
